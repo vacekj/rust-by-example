33ff4710d320f31f00698d8790545a03371c8250
Josef Vacek <vacekj@outlook.com>
2022-10-24 22:06:26 +0200

    feat(41): mutability

---
+++ src/ch42_scope_and_shadowing.rs
@@ -0,0 +1,4 @@
+fn main() {
+    // variable bindings have a scope, and are constrained to live in a block
+    // also, variable shadowing is (unfortunately) allowed
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod ch332_enums;
 mod ch333_constants;
 mod ch3custom_types;
 mod ch41_mutability;
+mod ch42_scope_and_shadowing;
 mod ch4_variable_bindings;
 
 use ch3custom_types::main as customtypesmain;
