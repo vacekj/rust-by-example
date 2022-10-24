05869fbcc016176f6bc6d04b5b78275fe30dd214
Josef Vacek <vacekj@outlook.com>
2022-10-24 21:59:34 +0200

    feat(4): variable bindings

---
+++ src/ch4_variable_bindings.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // silence unused variable warnings by prefixing the variable name with an underscore
+    let _unused_variable = "This is fine";
+    let unused_variable = "This is literally hell";
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod c222tuples;
 mod ch332_enums;
 mod ch333_constants;
 mod ch3custom_types;
+mod ch4_variable_bindings;
 
 use ch3custom_types::main as customtypesmain;
 
