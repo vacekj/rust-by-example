41b855bd0dbcbfd78f8a38fd9e7531f9e23c2a87
Josef Vacek <vacekj@outlook.com>
2022-10-26 13:27:25 +0200

    feat(51): casting

--- src/ch51_casting.rs
+++ src/ch51_casting.rs
@@ -5,5 +5,9 @@ pub fn main() {
     let decimal = 65.4321_f32;
 
     // Error! No implicit conversion
-    let integer: u8 = decimal;
+    // let integer: u8 = decimal;
+
+    // Explicit conversion
+    let integer = decimal as u8;
+    let character = integer as char;
 }
