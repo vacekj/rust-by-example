25916f26adc33392d0dc99580c4232a24545200f
Josef Vacek <vacekj@outlook.com>
2022-10-26 14:05:38 +0200

    feat(51): casting

--- src/ch51_casting.rs
+++ src/ch51_casting.rs
@@ -34,4 +34,14 @@ pub fn main() {
 
     // For positive numbers, this is the same as the modulus
     println!("1000 mod 256 is : {}", 1000 % 256);
+
+    // When casting to a signed type, the (bitwise) result is the same as
+    // first casting to the corresponding unsigned type. If the most significant
+    // bit of that value is 1, then the value is negative
+
+    // Unless it already fits, of course.
+    println!(" 128 as a i16 is: {}", 128 as i16);
+
+    // 128 as i8 -> -128, whose two's complement in eight bits is:
+    println!(" 128 as a i8 is : {}", 128 as i8);
 }
