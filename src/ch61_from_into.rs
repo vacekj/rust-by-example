58f20e2df1428d5bb6745d7bc62cd87c961b02df
Josef Vacek <vacekj@outlook.com>
2022-11-27 17:34:46 +0100

    feat(61): from into

---
+++ src/ch61_from_into.rs
@@ -0,0 +1,22 @@
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+fn main() {
+    /* From trait allows for a type to define how to create itself
+    from another type */
+    let my_str = "hello";
+    let my_string = String::from(my_str);
+
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+}
