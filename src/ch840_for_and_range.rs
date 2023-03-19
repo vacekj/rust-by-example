476ea9c06fa6153a4fbd30598237b1f2fe69f7a4
Josef Vacek <vacekj@outlook.com>
2023-03-19 11:39:01 +0100

    feat(840): for and range

--- src/ch840_for_and_range.rs
+++ src/ch840_for_and_range.rs
@@ -11,6 +11,8 @@ fn iter() {
 
     for name in names.iter() {
         match name {
+            /* the ampersand here signifies a borrow. we can't mutate and we are not moving the value
+            so it can be reused after returning from the loop */
             &"Ferris" => println!("There is a rustacean among us!"),
             _ => println!("Hello {}", name),
         }
@@ -19,3 +21,31 @@ fn iter() {
 
 // into_iter - consumes the collection, on each iteration the exact data is provided.
 // no longer available after because it has been moved within the loop
+
+fn into_iter() {
+    let names = vec!["Box", "Frank", "Ferris"];
+
+    for name in names.into_iter() {
+        match name {
+            /* the ampersand here signifies a borrow. we can't mutate and we are not moving the value
+            so it can be reused after returning from the loop */
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+}
+
+// iter_mut() - mutably borrows, allows collection to be modified in place
+
+fn iter_mut() {
+    let names = vec!["Box", "Frank", "Ferris"];
+
+    for name in names.iter_mut() {
+        match name {
+            /* the ampersand here signifies a borrow. we can't mutate and we are not moving the value
+            so it can be reused after returning from the loop */
+            &mut "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+}
