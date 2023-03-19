358165cec492b505fa1fc23a16bb5c1bc97d1ace
Josef Vacek <vacekj@outlook.com>
2023-03-19 11:28:40 +0100

    feat(840): for and range

--- src/ch840_for_and_range.rs
+++ src/ch840_for_and_range.rs
@@ -5,3 +5,17 @@
 // for loop will aply the into_iter function to the collection.
 // iter - borrows each element through each iteration, leaving the collection untouched
 // and available for reause after the loop
+
+fn iter() {
+    let names = vec!["Box", "Frank", "Ferris"];
+
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+}
+
+// into_iter - consumes the collection, on each iteration the exact data is provided.
+// no longer available after because it has been moved within the loop
