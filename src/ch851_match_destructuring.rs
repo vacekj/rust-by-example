2ba24ac5c652795d412b8fb4d367a07bba0d3354
Josef Vacek <vacekj@outlook.com>
2023-03-19 12:57:15 +0100

    feat(840): for and range

--- src/ch851_match_destructuring.rs
+++ src/ch851_match_destructuring.rs
@@ -45,4 +45,18 @@ fn pointersref() {
     let _not_a_reference = 3;
 
     let ref _is_a_reference = 4;
+
+    let value = 5;
+    let mut mut_value = 6;
+
+    match value {
+        ref r => println!("Got a reference: {:?}", r),
+    }
+
+    match mut_value {
+        ref mut m => {
+            // got a reference, gotta deref it before we can mutate
+            *m += 10;
+        }
+    }
 }
