f02866ade8bf583214340291e8147341eb8a727d
Josef Vacek <vacekj@outlook.com>
2023-03-19 11:25:57 +0100

    feat(840): for and range

--- src/ch840_for_and_range.rs
+++ src/ch840_for_and_range.rs
@@ -1,2 +1,7 @@
 // a..b yields values from a incluse to b _exclusive_
 // a..=b is inclusive to b
+
+// for and iterators:
+// for loop will aply the into_iter function to the collection.
+// iter - borrows each element through each iteration, leaving the collection untouched
+// and available for reause after the loop
