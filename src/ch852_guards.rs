0ca9ff7c03c4d81693f4aca7d9cd799b04f57959
Josef Vacek <vacekj@outlook.com>
2023-03-19 15:22:04 +0100

    feat(840): for and range

---
+++ src/ch852_guards.rs
@@ -0,0 +1,32 @@
+enum Temperature {
+    Celsius(i32),
+    FreedomUnits(i32),
+}
+
+// additional guards to a match arm
+
+fn main() {
+    let temp = Temperature::Celsius(35);
+
+    match temp {
+        Temperature::Celsius(t) if t > 30 => println!("{}C is above 30 Celsius", t),
+        Temperature::Celsius(t) => println!("{}C is below 30 Celsius", t),
+        Temperature::FreedomUnits(t) if t > 86 => println!("{}F is above 86 fahrenheit"),
+        _ => (),
+    }
+}
+
+// !!! compiler doesn't take guard conditions into account when doing
+// exhaustiveness checking
+
+fn exhaust() {
+    let number: u8 = 4;
+
+    match number {
+        i if i == 0 => println!("zero"),
+        i if i > 0 => println!("Greater than zero"),
+        _ => unreachable!(
+            "this will never happen, but the compiler doesn't know, since that's a runtime check"
+        ),
+    }
+}
